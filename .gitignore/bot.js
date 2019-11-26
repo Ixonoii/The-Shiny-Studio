a8d535d5c89cf2c5eab2b661a58238d3a9d13cd6
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-11-26 18:17:06 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -33,39 +33,3 @@ client.on('message', function(message){
         message.channel.send(pong_enbed)
     }
 })
-
-client.on('message', function (message) {
-    if (!message.guild) return
-    let args = message.content.trim().split(/ +/g)
-
-    if (args[0].toLocaleLowerCase()=== prefix + "report"){
-        let memberMEN = message.mentions.members.first()
-        let question = args.slice(2).join(" ")
-        let error1 = new Discord.RichEmbed()
-        .setTitle("You have to mention someone.")
-        .setColor(errorcolor)
-        let error2 = new Discord.RichEmbed()
-        .setTitle("You have to enter a reason.")
-        .setColor(errorcolor)
-        let error3 = new Discord.RichEmbed()
-        .setTitle("I can't send your report. I may not have the necessary permissions or the `` reports`` channel does not exist on this server.")
-        .setColor(errorcolor)
-        if(!memberMEN) return message.channel.send(error1)
-        if(!question) return message.channel.send(error2)
-        let reportembed = new Discord.RichEmbed()
-        .setTitle("A reported has been sent!")
-        .setColor(embedcolor)
-        .addField("Membre:", message.author.username + " (" + message.author.id + ")")
-        .addField("Membre reported:", memberMEN + " (" + memberMEN.id + ")")
-        .addField("Reason:", question)
-        .addField("Channel:", message.channel.name)
-        let reportconfirm = new Discord.RichEmbed()
-        .setTitle("Report sent!")
-        .setColor(successcolor)
-        let cChannel = message.guild.channels.find(c => c.name === "reports")
-        if(!cChannel) return message.channel.send(error3)
-    cChannel.send(reportembed);
-    message.delete();
-    message.channel.send(reportconfirm)
-}
-})
