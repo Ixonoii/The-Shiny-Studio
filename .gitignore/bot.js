bb49797baefe94d57f61733385e1806c42a50bdd
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-11-26 17:42:25 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -1,17 +1,21 @@
-                                                                 // CONFIGURATION //
-
 const Discord = require('discord.js');
 const client = new Discord.Client;
 var credits = "mBot - All rights reserved";
 var embedcolor = "#0584dd"
 var prefix = "-"
 
-                                                                 // BOT TOKEN //
-
 client.login(process.env.BOT_TOKEN)
-
-                                                                 // BOT STATUS //
 
 client.on('ready', function(){
     client.user.setActivity("-help | mBot", {type: "PLAYING"})
+})
+
+client.on('message', function(message){
+    if(message.content === "<@648551591623917578>"){
+        let replymessage = new Discord.RichEmbed()
+        .setTitle(":white_check_mark: Need help? Say -help!")
+        .setColor(embedcolor)
+        .setTimestamp(Date.now())
+        message.channel.send(replymessage)
+    }
 })
