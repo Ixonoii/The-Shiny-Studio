62211772f3f9165659b30e052e9d165055c47f75
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-11-27 12:27:03 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -9,27 +9,3 @@ client.login(process.env.BOT_TOKEN)
 client.on('ready', function(){
     client.user.setActivity("-help | mBot", {type: "PLAYING"})
 })
-
-// The bot will send an image if a user say -info //
-
-client.on('message', function(message){
-    if(message.content === "<@648551591623917578>"){
-        let replymessage = new Discord.RichEmbed()
-        .setTitle("My prefix is ``-``. Use -cmds to see all commands.")
-        .setColor(embedcolor)
-        message.channel.send(replymessage)
-    }
-})
-
-// The bot will send an image if a user say -info //
-
-client.on('message', function(message){
-    if(message.content === prefix + "avatar"){
-        var pong_enbed = new Discord.RichEmbed()
-        .setTitle("Here is your avatar, " + message.author.username + ".")
-        .setColor(embedcolor)
-        .setImage(message.author.displayAvatarURL)
-        .setURL(message.author.displayAvatarURL)
-        message.channel.send(pong_enbed)
-    }
-})
