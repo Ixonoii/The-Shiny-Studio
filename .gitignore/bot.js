537870514da4e479269cc77b63a729c5854f14cb
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-01 14:18:45 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -243,7 +243,7 @@ client.on('message', function (message) {
         let success = new Discord.RichEmbed()
         .setTitle(":white_check_mark: The bot is now offline!")
         .setColor(embedcolor)
-        client.user.setPresence({ game: { name: 'TEST GAME' }, status: "invisible" })
+        client.user.setPresence({ game: { name: 'TEST GAME' }, status: "dnd" })
         message.channel.send(success)
 }
 })
