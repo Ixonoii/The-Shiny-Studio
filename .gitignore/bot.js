d7d5f5a3107effbd581a37432f09a18784fa2fa3
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-11 19:40:43 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -11,26 +11,3 @@ client.login(process.env.BOT_TOKEN)
 client.on('ready', function(){
     client.user.setActivity(">> Bientôt Disponible <<", {type: "PLAYING"})
 })
-
-client.on('message', function(message){
-    if(message.content === "<@654374834888769556>"){
-        let prefixe = new Discord.RichEmbed()
-        .setTitle("Mon préfixe est ; sur tous les serveurs.")
-        .setColor(couleur)
-        message.channel.send(prefixe)
-    }
-})
-
-client.on('message', message =>{
-    if(message.content === prefix + "info"){
-        let embed = new Discord.RichEmbed()
-        .setTitle("Informations à propos du bot :")
-        .setColor(couleur)
-        .addField("Créateur :","Ixonoii#3216")
-        .addField("Nombres de commandes disponibles :","Bientôt disponible")
-        .addField("Serveurs :", client.guilds.get(Number))
-        .addField("Membres :", client.users.get(Number))
-        .addField("Version :", BOTversion)
-        message.channel.send(embed)
-    }
-})
