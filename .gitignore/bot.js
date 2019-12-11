0ffa732bf390b5df333e13f49c3c5d96a73ebefc
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-11 19:14:14 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -2,26 +2,20 @@
 
 const Discord = require('discord.js');
 const client = new Discord.Client;
-const fs = require('fs');
-var prefix = ";";
+const fs = require('fs')
 var couleur = "#00f0ff";
+
+client.login(process.env.BOT_TOKEN)
 
 client.on('ready', function(){
     client.user.setActivity(">> Bientôt Disponible <<", {type: "PLAYING"})
 })
 
-const warns = JSON.parse(fs.readFileSync('./warns.json'))
-
-client.login(process.env.BOT_TOKEN)
-
-
-                                                                 // BOT MENTION //
-
 client.on('message', function(message){
-    if(message.content === "Arplex est une pute xD"){
+    if(message.content === "test"){
         let mention = new Discord.RichEmbed()
         .setAuthor(message.author.displayAvatarURL, message.author.username)
-        .setTitle("mais moi je suis pas gay. :)")
+        .setTitle(":white_check_mark")
         .setColor(couleur)
         message.channel.send(mention)
     }
