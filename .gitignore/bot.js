677c5b1599451b78f0e3d4107cc8b3954a1eafab
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-11 19:03:26 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -13,3 +13,17 @@ client.on('ready', function(){
 const warns = JSON.parse(fs.readFileSync('./warns.json'))
 
 client.login(process.env.BOT_TOKEN)
+
+
+                                                                 // BOT MENTION //
+
+client.on('message', function(message){
+    if(message.content === "<@654374834888769556>"){
+        if(!message.author.id === "434061967951659019") return message.channel.send("Vous n'êtes pas mon créateur.")
+        let réponse = new Discord.RichEmbed()
+        .setAuthor(message.author.displayAvatarURL, message.author.username)
+        .setTitle("Besoin d'aide ?")
+        .setColor(couleur)
+        .setDescription("Test!")
+    }
+})
