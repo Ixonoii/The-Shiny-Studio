0d27022f2bc6c5fee7869d68dcdb69aff690e526
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-12 19:09:41 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -11,3 +11,13 @@ client.login(process.env.BOT_TOKEN)
 client.on('ready', function(){
     client.user.setActivity(">> Bientôt Disponible <<", {type: "PLAYING"})
 })
+
+client.on('message', message =>{
+    if(message.content === prefix + "aide"){
+        let embed = new Discord.RichEmbed()
+        .setTitle("Voici les différentes aides disponibles :")
+        .setColor(couleur)
+        .setDescription(";aide commandes ``Affiche toutes les commandes disponibles.`` \n ;aide configuration ``Affiche des informations sur les rôles.")
+        message.channel.send(embed)
+    }
+})
