b63a1ec1b7babf635532b64010ccd739252d9b6d
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-12 19:11:01 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client;
 const fs = require('fs')
 var couleur = "#00f0ff";
-var BOTversion = "BETA"
+var prefix = ";"
 
 client.login(process.env.BOT_TOKEN)
 
