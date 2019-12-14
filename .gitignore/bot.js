505496fed68857f63d4295a478a19a5c0c817a8b
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-14 16:45:05 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -27,7 +27,7 @@ client.on('message', function(message){
     if(message.content === prefix + "cmds"){
         var pong_enbed = new Discord.RichEmbed()
         .setColor(couleur)
-        .addField("**__Commandes publiques :__**",";aide ``Affiche plusieurs aides.`` \n ;cmds/commandes ``Affiche les commandes disponibles.`` \n ;avatar ``Affiche votre photo de profil Discord.`` \n ;signal `` Signal un membre du serveur.`` \n ;bug `` Signal un bug à notre équipe de développeurs. \n ;info `` Affiche plusieurs informations à propos d'un membre.`` \n ;8ball `` Répond à vos questions.`` \n ;serveur `` Affiche plusieurs informations à propos d'un serveur.``")
+        .addField("**__Commandes publiques :__**",";aide ``Affiche plusieurs aides.`` \n ;cmds/commandes ``Affiche les commandes disponibles.`` \n ;avatar ``Affiche votre photo de profil Discord.`` \n ;signal ``Signal un membre du serveur.`` \n ;bug `` Signal un bug à notre équipe de développeurs.`` \n ;info ``Affiche plusieurs informations à propos d'un membre.`` \n ;8ball `` Répond à vos questions.`` \n ;serveur `` Affiche plusieurs informations à propos d'un serveur.``")
         .addField("**__Commandes d'administration :__**",";kick ``Expulse un membre.`` \n ;ban ``Ban un membre.`` \n ;softban ``Ban puis un-ban un membre.`` \n ;supprime ``Supprime un grand nombre de messages.`` \n ;mute ``Rend un membre muet.`` \n ;un-mute ``Rend un membre un-muet.`` \n ;pseudo ``Renomme un membre.``")
         .addField("**__Commandes de gestion :__**",";rôle ``Créé un nouveau rôle.`` \n ;renomme ``Renomme un channel.`` \n ;sujet ``Définie le sujet d'un channel.`` \n ;nomduserveur ``Renomme un serveur.``")
         .setFooter("Plus de commandes très bientôt !")
@@ -46,7 +46,7 @@ client.on('message', function(message){
     if(message.content === prefix + "commandes"){
         var pong_enbed = new Discord.RichEmbed()
         .setColor(couleur)
-        .addField("**__Commandes publiques :__**",";aide ``Affiche plusieurs aides.`` \n ;cmds/commandes ``Affiche les commandes disponibles.`` \n ;avatar ``Affiche votre photo de profil Discord.`` \n ;signal `` Signal un membre du serveur.`` \n ;bug `` Signal un bug à notre équipe de développeurs. \n ;info `` Affiche plusieurs informations à propos d'un membre.`` \n ;8ball `` Répond à vos questions.`` \n ;serveur `` Affiche plusieurs informations à propos d'un serveur.``")
+        .addField("**__Commandes publiques :__**",";aide ``Affiche plusieurs aides.`` \n ;cmds/commandes ``Affiche les commandes disponibles.`` \n ;avatar ``Affiche votre photo de profil Discord.`` \n ;signal ``Signal un membre du serveur.`` \n ;bug `` Signal un bug à notre équipe de développeurs.`` \n ;info ``Affiche plusieurs informations à propos d'un membre.`` \n ;8ball `` Répond à vos questions.`` \n ;serveur `` Affiche plusieurs informations à propos d'un serveur.``")
         .addField("**__Commandes d'administration :__**",";kick ``Expulse un membre.`` \n ;ban ``Ban un membre.`` \n ;softban ``Ban puis un-ban un membre.`` \n ;supprime ``Supprime un grand nombre de messages.`` \n ;mute ``Rend un membre muet.`` \n ;un-mute ``Rend un membre un-muet.`` \n ;pseudo ``Renomme un membre.``")
         .addField("**__Commandes de gestion :__**",";rôle ``Créé un nouveau rôle.`` \n ;renomme ``Renomme un channel.`` \n ;sujet ``Définie le sujet d'un channel.`` \n ;nomduserveur ``Renomme un serveur.``")
         .setFooter("Plus de commandes très bientôt !")
@@ -65,31 +65,11 @@ client.on('message', function(message){
     if(message.content === prefix + "aide autorisations"){
         var pong_enbed = new Discord.RichEmbed()
         .setColor(couleur)
-        .addField("**__Commandes publiques :__**",";aide ``Affiche plusieurs aides.`` \n ;cmds/commandes ``Affiche les commandes disponibles.`` \n ;avatar ``Affiche votre photo de profil Discord.`` \n ;signal `` Signal un membre du serveur.`` \n ;bug `` Signal un bug à notre équipe de développeurs. \n ;info `` Affiche plusieurs informations à propos d'un membre.`` \n ;8ball `` Répond à vos questions.`` \n ;serveur `` Affiche plusieurs informations à propos d'un serveur.``")
+        .addField("**__Commandes publiques :__**",";aide ``Affiche plusieurs aides.`` \n ;cmds/commandes ``Affiche les commandes disponibles.`` \n ;avatar ``Affiche votre photo de profil Discord.`` \n ;signal ``Signal un membre du serveur.`` \n ;bug `` Signal un bug à notre équipe de développeurs.`` \n ;info ``Affiche plusieurs informations à propos d'un membre.`` \n ;8ball `` Répond à vos questions.`` \n ;serveur `` Affiche plusieurs informations à propos d'un serveur.``")
         .addField("**__Commandes d'administration :__**",";kick ``Expulse un membre.`` \n ;ban ``Ban un membre.`` \n ;softban ``Ban puis un-ban un membre.`` \n ;supprime ``Supprime un grand nombre de messages.`` \n ;mute ``Rend un membre muet.`` \n ;un-mute ``Rend un membre un-muet.`` \n ;pseudo ``Renomme un membre.``")
         .addField("**__Commandes de gestion :__**",";rôle ``Créé un nouveau rôle.`` \n ;renomme ``Renomme un channel.`` \n ;sujet ``Définie le sujet d'un channel.`` \n ;nomduserveur ``Renomme un serveur.``")
         .setFooter("Plus de commandes très bientôt !")
         message.channel.send(pong_enbed)
-        let serverlog = new Discord.RichEmbed()
-        .setAuthor(message.author.tag, message.author.displayAvatarURL)
-        .setColor(couleur)
-        .addField("**__Commande :__**",";aide autorisations")
-        .addField("**__Utilisateur :__**", message.author.tag + " | " + message.author.id)
-        .addField("**__Serveur :__**", message.guild.name + " | " + message.guild.id)
-        client.channels.get("654757180037267516").send(serverlog);
-    }
-})
-
-client.on('message', message =>{
-    if(message.content === prefix + "aide autorisations"){
-        let embed = new Discord.RichEmbed()
-        .setTitle("Informations sur les autorisations :")
-        .setColor(couleur)
-        .setDescription("Certaines commandes de bot nécessitent des autorisations spéciales pour fonctionner. C'est pourquoi un membre devra avoir un de ces rôles pour utiliser une commande.")
-        .addField("**__Modérateur :__**","Une personne possédant ce rôle pourra utiliser des commandes de base comme ;ban, ;kick, ;mute, ;supprime, et plus.")
-        .addField("**__Administrateur :__**","Une personne possédant ce rôle pourra utiliser des commandes de gestion comme ;description, ;nouveaurole, ;bloque, ;débloque, et plus.")
-        .addField("**__Super Administrateur :__**","Une personne possédant ce rôle pourra utiliser toutes les commandes disponibles.")
-        message.channel.send(embed)
         let serverlog = new Discord.RichEmbed()
         .setAuthor(message.author.tag, message.author.displayAvatarURL)
         .setColor(couleur)
