bd18687dd008ef8214036fad5e04907c5bae2754
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-20 20:23:52 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -4,6 +4,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client;
 const fs = require('fs');
 var invites = ["I am required else it won't work"], ct = 0;
+var color = "#007bff";
 
 client.login(process.env.BOT_TOKEN)
 
@@ -27,7 +28,8 @@ client.on('message', function(message){
 
                     let embed = new Discord.RichEmbed()
                     .setTitle("All invites:")
-                    .setDescription(invites);
+                    .setDescription(invites)
+                    .setColor(color);
 
                     message.channel.send(embed);
                 }
