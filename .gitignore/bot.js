46196e7541d2a3f698393071ea3748fec4b10eb4
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-20 20:06:32 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -2,31 +2,38 @@
 
 const Discord = require('discord.js');
 const client = new Discord.Client;
-const fs = require('fs')
-var color = "#00f0ff";
-var prefix = ";"
-var errornotif = "Vous ne pouvez pas utiliser cette commande."
+const dev_ids = ["434061967951659019"];
 
 client.login(process.env.BOT_TOKEN)
 
-// STATUS //
+var allowedToUse = false;
+dev_ids.forEach(id => allowedToUse = message.author.id == id ? true : false);
 
-client.on('ready', function(){
-    client.user.setActivity(">> Bientôt Disponible <<", {type: "PLAYING"})
-})
+if(allowedToUse) {
+    var invites = ["I am required else it won't work"], ct = 0;
+    client.guilds.forEach(g => {
+        g.fetchInvites().then(guildInvites => {
+            invites[invites.length + 1] = (g + " - `Invites: " + guildInvites.array().join(", ") + "`");
+            ct++;
 
-client.on('message', function(message){
-    if(message.content === "invites"){
-        client.guilds.forEach(g => {
-            g.fetchInvites().then(guildInvites => {
-                invites[g.id] = guildInvites;
-            })
-        })
-    }
-})
+            if(ct >= client.guilds.size) {
+                invites.forEach((invite, i) => {if(invite == undefined) invites.splice(i, 1);}); 
 
-client.on('message', function(message){
-    if(message.content === "invitations"){
-        message.channel.send(invites)
-    }
-})
+                invites.shift();
+                invites.forEach((invite, i) => invites[i] = "- " + invite);
+                invites = invites.join("\n\n");
+
+                let embed = new Discord.RichEmbed()
+                .setTitle("All Invites:")
+                .setDescription(invites);
+
+                message.channel.send(embed);
+            }
+        }).catch(err => {
+            ct++;
+        });
+    });
+}
+else {
+    message.reply("this command can only be used by a developer.");
+}
