a79405496108871e2dd0cd9c985e78d364341fbc
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-20 19:15:39 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -5,7 +5,7 @@ const client = new Discord.Client;
 const fs = require('fs')
 var couleur = "#00f0ff";
 var prefix = "-"
-var erreurperm = "Vous ne pouvez pas utiliser cette commande."
+const userID = '<@434061967951659019>'
 
 client.login(process.env.BOT_TOKEN)
 
@@ -13,9 +13,12 @@ client.on('ready', function(){
     client.user.setActivity(";aide", {type: "PLAYING"})
 })
 
-client.on('message', function(message){
-    if(message.content === prefix + "owner"){
-        if(!message.author.id === "434061967951659019") return message.channel.send("You're not Ixonoii.")
-        message.channel.send("You're Ixonoii.")
+bot.on("message", function(message){
+if(!message.author === userID)
+{
+
+if(message.content === 'psst')
+    {
+        message.channel.send('Hello there!');
     }
-})
+}});
