487dab11ff2d04e293a975a46f6544f7cb1aa5e6
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-20 19:26:57 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -9,18 +9,5 @@ var prefix = "-"
 client.login(process.env.BOT_TOKEN)
 
 client.on('ready', function(){
-    client.user.setActivity(";aide", {type: "PLAYING"})
-})
-
-client.on('message', function (message) {
-    if (!message.guild) return
-    let args = message.content.trim().split(/ +/g)
- 
-    if (args[0].toLowerCase() === prefix + 'dm') {
-       let member = args.slice(1).join(" ")
-       let reason = args.slice(2).join(" ")
-       if (!member) return message.channel.send("**Vous devez mentionner quelqu'un.**")
-       if (!reason) return message.channel.send("**Vous devez entrer une raison.**")
-       client.users.get(member).send(reason)
-    }
+    client.user.setActivity("Bientôt Disponible", {type: "PLAYING"})
 })
