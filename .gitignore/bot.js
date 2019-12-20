ddf1f03088899d7ab557072596eaa08a21250005
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-20 19:16:53 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -5,20 +5,9 @@ const client = new Discord.Client;
 const fs = require('fs')
 var couleur = "#00f0ff";
 var prefix = "-"
-const userID = '<@434061967951659019>'
 
 client.login(process.env.BOT_TOKEN)
 
 client.on('ready', function(){
     client.user.setActivity(";aide", {type: "PLAYING"})
 })
-
-bot.on("message", function(message){
-if(!message.author === userID)
-{
-
-if(message.content === 'psst')
-    {
-        message.channel.send('Hello there!');
-    }
-}});
