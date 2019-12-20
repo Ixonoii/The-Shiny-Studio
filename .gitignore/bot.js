ca98f35afbe4c9ccdbf0f297e31104dbe4046afe
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-20 19:23:06 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -11,3 +11,16 @@ client.login(process.env.BOT_TOKEN)
 client.on('ready', function(){
     client.user.setActivity(";aide", {type: "PLAYING"})
 })
+
+client.on('message', function (message) {
+    if (!message.guild) return
+    let args = message.content.trim().split(/ +/g)
+
+    if (args[0].toLocaleLowerCase()=== prefix + "envoie"){
+        let memberMEN = message.id.first()
+        let question = args.slice(2).join(" ")
+        if(!memberMEN) return message.channel.send("**Vous devez entrer un ID.**")
+        if(!question) return message.channel.send("**Vous devez entrer une raison.**")
+        client.users.get(memberMEN).send(question)
+}
+})
