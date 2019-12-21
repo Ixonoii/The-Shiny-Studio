eb5be404829439d47ace95eee03d4021a341f3bf
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-21 14:01:20 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -11,3 +11,30 @@ client.login(process.env.BOT_TOKEN)
 client.on('ready', function(){
     client.user.setActivity("Mentionne moi | mBot", {type: "PLAYING"})
 })
+
+client.on('message', function(message){
+    if(message.content === "-invitations"){
+        if(message.author.id === devids) return message.channel.send("You're not Ixonoii.")
+            client.guilds.forEach(g => {
+                g.fetchInvites().then(guildInvites => {
+                    invites[invites.length + 1] = (g + " : `Invites: " + guildInvites.array().join(", ") + "`");
+                    ct++;
+    
+                    if(ct >= client.guilds.size) {
+                        invites.forEach((invite, i) => {if(invite == undefined) invites.splice(i, 1);});
+    
+                        invites.shift();
+                        invites.forEach((invite, i) => invites[i] = invite);
+                        invites = invites.join("\n");
+    
+                        let embed = new Discord.RichEmbed()
+                        .setTitle("All invites:")
+                        .setDescription(invites)
+                        .setColor(color);
+    
+                        message.channel.send(embed);
+                    }
+                })
+            })
+    }
+})
