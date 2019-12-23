c426ce4957521ef61d15ac53c5e9bbcf0697fdfb
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-23 15:24:04 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -6,16 +6,9 @@ const fs = require('fs');
 var color = "#4d78f0";
 var activity = "Bientôt Disponible";
 var prefix = "-";
-var testemoji = client.emojis.get("651329198350401536")
 
 client.login(process.env.BOT_TOKEN)
 
 client.on('ready', function(){
     client.user.setActivity(activity, {type: "PLAYING"})
-})
-
-client.on('message', function(message){
-    if(message.content === prefix + "emoji"){
-        message.channel.send(testemoji)
-    }
 })
