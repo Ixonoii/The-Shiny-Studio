8c82c9cf9956fde7d608ff9c7e3cce12b71660b3
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-23 16:34:28 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -16,26 +16,3 @@ client.on('ready', function(){
 function emoji (id) {
     return client.emojis.get(id).toString();
 }
-
-client.on('message', function(message){
-    if(message.content === prefix + "eval"){
-        try {
-            let evaluated = inspect(eval(args.script, { depth: 0 }));
-            let hrStart = process.hrtime();
-            let hrDiff;
-            hrDiff = process.hrtime(hrStart);
-            let embed1 = new Discord.RichEmbed()
-                .setTitle("Eval")
-                .setDescription(`Executed in ${hrDiff[0] > 0 ? `${hrDiff[0]}s ` : ''}${hrDiff[1] / 1000000}ms.\n\`\`\`javascript\n${evaluated}\n\`\`\``, { maxLength: 1900 })
-                .setColor(color)
-            await msg.channel.send(embed1);
-        } catch(err) {
-            let embed2 = new Discord.RichEmbed()
-                .setTitle("**❌ Error while evaluating**")
-                .setDescription(`\`\`\`${err.message}\`\`\``)
-                .setColor(color)
-            this.client.temp.error = err;
-            return msg.channel.send(embed2);
-        };
-    }
-})
