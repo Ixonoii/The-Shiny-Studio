860fcb5f4643c04df4bc8f2376a32f825cbb028d
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-23 16:21:17 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -4,9 +4,8 @@ const Discord = require("discord.js");
 const client = new Discord.Client;
 const fs = require('fs');
 var color = "#4d78f0";
-var activity = "Updated !";
+var activity = "Updated!";
 var prefix = "-";
-var ownerid = "434061967951659019"
 
 client.login(process.env.BOT_TOKEN)
 
@@ -18,12 +17,10 @@ function emoji (id) {
     return client.emojis.get(id).toString();
 }
 
-client.on("message", function(message) {
-    if (message.content === prefix + "check") {
-        if (!message.author.id === ownerid) {
-            return message.channel.send(emoji("641689428565164063") + " **You're not Ixonoii.**");
-        } else {
-            return message.channel.send(emoji("641771906726625299") + " **You are Ixonoii.**");
-        };
-    };
+client.on('message', function(message) {
+    if(message.content === prefix + "check"){
+        if(message.author.id === "434061967951659019") return ( emoji("641689428565164063") + " **You're not Ixonoii.**")
+    } else {
+        message.channel.send( emoji("641771906726625299") + " **You are Ixonoii.**")
+}
 });
