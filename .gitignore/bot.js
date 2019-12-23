0cf46f21769a980f4b92ffef31a75c7bc996a7ca
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-23 16:11:04 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -4,7 +4,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client;
 const fs = require('fs');
 var color = "#4d78f0";
-var activity = "Updated!!";
+var activity = "Updated!";
 var prefix = "-";
 
 client.login(process.env.BOT_TOKEN)
@@ -21,7 +21,8 @@ client.on("message", function(message) {
     if (message.content === prefix + "check") {
         if (!message.author.id === "434061967951659019") {
             return message.channel.send(emoji("641689428565164063") + " **You're not Ixonoii.**");
+        } else {
+            return message.channel.send(emoji("641771906726625299") + " **You are Ixonoii.**");
         };
-        message.channel.send(emoji("641771906726625299") + " **You are Ixonoii.**");
     };
 });
