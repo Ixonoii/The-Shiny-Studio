20c1153dc39f81d233563dfd9ca7337b233c342e
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-23 15:05:16 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -4,10 +4,22 @@ const Discord = require("discord.js");
 const client = new Discord.Client;
 const fs = require('fs');
 var color = "#4d78f0";
-var activity = "Bientôt Disponible"
+var activity = "Bientôt Disponible";
+var prefix = "-";
 
 client.login(process.env.BOT_TOKEN)
 
 client.on('ready', function(){
     client.user.setActivity(activity, {type: "PLAYING"})
+})
+
+client.on('message', function(message){
+    if(message.content === prefix + "moi"){
+        let embedtest = new Discord.RichEmbed()
+        .setTitle("Voici quelques informations à propos de l\"utilisateur " + message.author.username)
+        .addField("Nom d'utilisateur :", message.author.username)
+        .addField("ID :", message.author.id, true)
+        .addField("Arrivé le :", message.author.createdAt, true)
+        message.channel.send(embedtest)
+    }
 })
