e0ebb4b4e0b9790812cc61e6d8bddc571f1c056e
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-23 15:37:13 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 var color = "#4d78f0";
 var activity = "Bientôt Disponible";
 var prefix = "-";
-var testemoji = client.emojis.get("651329198350401536")
+var blacklistedIDs = [""]
 
 client.login(process.env.BOT_TOKEN)
 
@@ -19,7 +19,20 @@ function emoji (id) {
 }
 
 client.on('message', function(message){
-    if(message.content === "test"){
-        message.channel.send( emoji("641771906726625299") );
+    if(message.content === prefix + "blacklist"){
+        message.channel.send( emoji("641771906726625299") + "here is the list: " + blacklistedIDs);
     }
+})
+
+client.on('message', function (message) {
+    if (!message.guild) return
+    let args = message.content.trim().split(/ +/g)
+
+    if (args[0].toLocaleLowerCase()=== prefix + "blacklist"){
+        let memberMEN = message.mentions.members.first()
+        if(!memberMEN) return message.channel.send( emoji("641689428565164063") + " You have to mention someone.")
+    message.delete();
+    message.channel.send( emoji('641771906726625299') + memberMEN + " has been blacklisted from using the bot!")
+    blacklistedIDs = blacklistedIDs + memberMEN.id
+}
 })
