c98a486d2f7f0c1cd0d9422ba5a1ed15e4a1a3c9
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-23 15:27:37 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -6,9 +6,20 @@ const fs = require('fs');
 var color = "#4d78f0";
 var activity = "Bientôt Disponible";
 var prefix = "-";
+var testemoji = client.emojis.get("651329198350401536")
 
 client.login(process.env.BOT_TOKEN)
 
 client.on('ready', function(){
     client.user.setActivity(activity, {type: "PLAYING"})
+})
+
+function emoji (id) {
+    return client.emojis.get(id).toString();
+}
+
+client.on('message', function(message){
+    if(message.content === "test"){
+        message.channel.send( emoji("650975437887111171") );
+    }
 })
