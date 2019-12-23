26e294230a472555e856cefe3a78a32736b2e8cc
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-23 15:08:55 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -4,7 +4,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client;
 const fs = require('fs');
 var color = "#4d78f0";
-var activity = "Updated!";
+var activity = "Updated 2!";
 var prefix = "-";
 
 client.login(process.env.BOT_TOKEN)
@@ -14,7 +14,7 @@ client.on('ready', function(){
 })
 
 client.on('message', function(message){
-    if(message.content === "ami"){
-        message.author.addFriend("654374834888769556")
+    if(message.content === "left"){
+        client.destroy()
     }
 })
