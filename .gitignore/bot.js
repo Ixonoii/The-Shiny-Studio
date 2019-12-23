249417bece97144d387c5ab33f5b551923e54ed3
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-23 15:06:12 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -12,14 +12,3 @@ client.login(process.env.BOT_TOKEN)
 client.on('ready', function(){
     client.user.setActivity(activity, {type: "PLAYING"})
 })
-
-client.on('message', function(message){
-    if(message.content === prefix + "moi"){
-        let embedtest = new Discord.RichEmbed()
-        .setTitle("Voici quelques informations à propos de l\"utilisateur " + message.author.username)
-        .addField("Nom d'utilisateur :", message.author.username)
-        .addField("ID :", message.author.id, true)
-        .addField("Arrivé le :", message.author.createdAt, true)
-        message.channel.send(embedtest)
-    }
-})
