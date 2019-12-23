7c0060e120e6eb9087ac18faf49a09193798d893
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-23 14:59:09 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -3,11 +3,11 @@
 const Discord = require("discord.js");
 const client = new Discord.Client;
 const fs = require('fs');
-var color = "#007bff";
-var prefix = "-";
+var color = "#4d78f0";
+var activity = "Bientôt Disponible"
 
 client.login(process.env.BOT_TOKEN)
 
 client.on('ready', function(){
-    client.user.setActivity("Bientôt Disponible", {type: "PLAYING"})
+    client.user.setActivity(activity, {type: "PLAYING"})
 })
