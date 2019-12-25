ef9b5cdb68bde4edb43bc7f84037954425c29481
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-25 16:31:14 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -4,14 +4,9 @@ const Discord = require("discord.js");
 const client = new Discord.Client;
 const fs = require('fs');
 var color = "#4d78f0";
-var activity = "with Arplex Premium";
 var prefix = "-";
 
 client.login(process.env.BOT_TOKEN)
-
-client.on('ready', function(){
-    client.user.setActivity(activity, {type: "PLAYING"})
-})
 
 function emoji (id) {
     return client.emojis.get(id).toString();
