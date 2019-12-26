12028dd4ca1f96649b6475a7f542b44ca49ef3f0
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-26 20:11:31 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -11,3 +11,11 @@ client.login(process.env.BOT_TOKEN)
 function emoji (id) {
     return client.emojis.get(id).toString();
 }
+
+client.on('message', function(message){
+    if(message.content === "<@" + botID + ">"){
+        let prefixembed = new Discord.RichEmbed()
+        .setTitle("Mon préfixe est ``" + prefix + "``.")
+        message.channel.send(prefix)
+    }
+})
