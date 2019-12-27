ed9a5b32fe78ae48bdd2fc62dbecddc24907ab6f
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-27 18:42:25 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -13,11 +13,30 @@ function emoji (id) {
     return client.emojis.get(id).toString();
 }
 
-client.on('message', function(message){
-    if(message.content === "<@650067878292357170>"){
-        let prefixembed = new Discord.RichEmbed()
-        .setTitle("Mon préfixe est ``" + prefix + "``.")
-        .setColor(successcolor)
-        message.channel.send(prefix)
-    }
+client.on('message', function (message) {
+    if (!message.guild) return
+    let args = message.content.trim().split(/ +/g)
+
+    if (args[0].toLowerCase() === prefix + "support") {
+        let notallowed = new Discord.RichEmbed()
+        .setTitle( emoji("659504785036148750") + " Vous ne disposez pas des autorisations nécessaires pour utiliser cette commande.")
+        .setColor(errorcolor)
+        let nomention = new Discord.RichEmbed()
+        .setTitle( emoji("659504785036148750") + " Vous devez mentionner quelqu'un.")
+        .setColor(errorcolor)
+        if(!message.member.roles.some(r=>["Développeur"].includes(r.name)) ) return message.channel.send(notallowed)
+        let member = message.mentions.members.first()
+        if (!member) return message.channel.send(nomention)
+        let supportrole = message.guild.roles.find(role => role.name === 'Support')
+        let staffrole = message.guild.roles.find(role => role.name === 'Staff')
+        if (staffrole) {
+            member.addRole(staffrole)
+        }
+        if (supportrole) {
+            member.addRole(supportrole)
+            let success = new Discord.RichEmbed()
+            .setTitle( emoji("659504835535831060") + member + " rejoint l'équipe d'assistance !")
+            .setColor(errorcolor)
+            message.channel.send(success)
+        }}
 })
