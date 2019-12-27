651a24591ea5d88b09dc50bf609ad8a681911578
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-27 18:27:25 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -3,7 +3,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client;
 const fs = require('fs');
-var color = "#4d78f0";
+var successcolor = "#4d78f0";
+var errorcolor = "#ff0000"
 var prefix = "!";
 
 client.login(process.env.BOT_TOKEN)
@@ -13,9 +14,10 @@ function emoji (id) {
 }
 
 client.on('message', function(message){
-    if(message.content === "<@" + botID + ">"){
+    if(message.content === "<@650067878292357170>"){
         let prefixembed = new Discord.RichEmbed()
         .setTitle("Mon préfixe est ``" + prefix + "``.")
+        .setColor(successcolor)
         message.channel.send(prefix)
     }
 })
