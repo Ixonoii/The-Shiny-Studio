c8b9607d4021ebb75281d64d90f6e49c4768bd49
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2020-01-12 17:21:47 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -23,9 +23,37 @@ client.on('ready', function(){
 
 // ----------------------------------------- TEST ----------------------------------------- //
 
-client.on("message", message=>{
-    var helpembed = new Discord.RichEmbed()
-    .setTitle(notallowedmessage)
-    if(message.content === prefix + "remove")
-    message.channel.send(helpembed)
+client.on('message', function (message) {
+    if (!message.guild) return
+    let args = message.content.trim().split(/ +/g)
+ 
+    if (args[0].toLowerCase() === prefix + 'kick') {
+        var notallowed = new Discord.RichEmbed()
+        .setTitle(notallowedmessage)
+        .setTimestamp()
+        var nomention = new Discord.RichEmbed()
+        .setTitle("Veuillez mentionner un utilisateur.")
+        .setTimestamp()
+        var noreason = new Discord.RichEmbed()
+        .setTitle("Veuillez entrer une raison.")
+        .setTimestamp()
+        var cantkickowner = new Discord.RichEmbed()
+        .setTitle("Vous ne pouvez pas kick cet utilisateur.")
+        .setTimestamp()
+        var nokickable = new Discord.RichEmbed()
+        .setTitle("Je ne peux pas kick cet utilisateur.")
+        .setTimestamp()
+       if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send(notallowed)
+       let member = message.mentions.members.first()
+       let reason = args.slice(1).join(" ")
+       if (!member) return message.channel.send(nomention)
+       if (!reason) return message.channel.send(noreason)
+       if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.owner.id) return message.channel.send(cantkickowner)
+       if (!member.kickable) return message.channel.send(nokickable)
+       member.kick()
+       var success = new Discord.RichEmbed()
+        .setTitle(member.displayName + " a été kick du serveur par " + message.author + " pour la raison suivante : " + reason)
+        .setTimestamp()
+       message.channel.send(success)
+    }
 })
