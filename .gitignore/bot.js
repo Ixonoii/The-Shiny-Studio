0447cc43f71afe9ba7095314a1015a9c01095135
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2020-01-12 14:35:29 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -1,13 +1,27 @@
-// CONFIGURATION //
+// ----------------------------------------- SETTINGS ----------------------------------------- //
 
 const Discord = require("discord.js");
 const client = new Discord.Client;
 const fs = require('fs');
-var color = "#4d78f0";
-var prefix = "-";
+var prefix = ";";
+var status = "Test";
+var errorlogo = "659504785036148750";
+var successlogo = "659504835535831060";
+var notallowedmessage = "Vous ne disposez pas des autorisations nécessaires pour utiliser cette commande.";
 
 client.login(process.env.BOT_TOKEN)
 
 function emoji (id) {
     return client.emojis.get(id).toString();
 }
+
+client.on('ready', function(){
+    client.user.setActivity(status, {type: "PLAYING"})
+})
+
+// ----------------------------------------- TEST ----------------------------------------- //
+
+client.on("message", message=>{
+    if(message.content === prefix + "test")
+    message.channel.send(emoji(successlogo))
+})
