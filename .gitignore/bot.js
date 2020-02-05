ba8334531ebb1bb9b59235b4072cb3bdda03fe94
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2020-02-05 12:38:23 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -5,9 +5,9 @@
 const Discord = require("discord.js");
 const client = new Discord.Client;
 const fs = require('fs');
-var status = "Test";
+const Trello = require("trello")
+var trello = new Trello("aff42b030b2c9e009b9fa35dd171faf8", "2541baf0c4f5b7f7b63f62578b1ccbb2a91fbcfff9d326fa48857f68ab380b6c");
 var prefix = "-";
-
 client.login(process.env.TOKENBOT)
 
 // ---------------------------------------------------------------------------------------- //
@@ -18,9 +18,21 @@ client.on('message', function (message) {
     if (!message.guild) return
     let args = message.content.trim().split(/ +/g)
  
-    if (args[0].toLowerCase() === prefix + 'addban') {
-       let PlayerBanned = args.slice(1).join(" ")
-       if (!PlayerBanned) return message.channel.send(":x: An error occurred.")
-       message.channel.send("T!createcard Banlist | " + PlayerBanned)
+    if (args[0].toLowerCase() === prefix + 'ban') {
+        var notallowed = new Discord.RichEmbed()
+        .setTitle(":x: You're not allowed to use this command.")
+        var nomention = new Discord.RichEmbed()
+        .setTitle(":x: Please enter a username.")
+        var noreason = new Discord.RichEmbed()
+        .setTitle(":x: Please enter a reason.")
+       let member = args.slice(1)
+       let reason = args.slice(2).join(" ")
+       if (!member) return message.channel.send(nomention)
+       if (!reason) return message.channel.send(noreason)
+       trello.addCard(member, reason)
+       var success = new Discord.RichEmbed()
+        .setTitle(":white_check_mark: " + member + " has been banned from the game, and added in the Trello ban list.")
+        .setTimestamp()
+       message.channel.send(success)
     }
 })
