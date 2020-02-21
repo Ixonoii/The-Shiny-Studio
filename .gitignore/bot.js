da9728e0505fa935fe3d9f2d2da07b81af64a162
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2020-02-21 13:25:20 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -17,9 +17,12 @@ client.on("message", function (message) {
     if (!message.guild) return
     let args = message.content.trim().split(/ +/g)
  
-    if (args[0].toLowerCase() === prefix + "test") {
-        var cmdsembed = new Discord.RichEmbed()
-        .setTitle("Arlenox is a noob.")
-        message.channel.send(cmdsembed)
+    if (args[0].toLowerCase() === prefix + "group") {
+        var GroupEmbed = new Discord.RichEmbed()
+        .setAuthor(message.author.displayAvatarURL, message.author.tag)
+        .setTitle("The Shiny Studio")
+        .setDescription("Our Roblox group is a vital part of the game. Our group is the way you get access to the discord and much more. There will be possible perks if you are in the group coming soon.")
+        .setURL("https://www.roblox.com/groups/5063409/The-Shiny-Studio#!/about")
+        message.channel.send(GroupEmbed)
     }
 })
