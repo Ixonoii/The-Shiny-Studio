884a72f2f09f63cea40ba954e4ea1629899cc6c4
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2020-02-21 13:26:39 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -19,7 +19,6 @@ client.on("message", function (message) {
  
     if (args[0].toLowerCase() === prefix + "group") {
         var GroupEmbed = new Discord.RichEmbed()
-        .setAuthor(message.author.displayAvatarURL, message.author.tag)
         .setTitle("The Shiny Studio")
         .setDescription("Our Roblox group is a vital part of the game. Our group is the way you get access to the discord and much more. There will be possible perks if you are in the group coming soon.")
         .setURL("https://www.roblox.com/groups/5063409/The-Shiny-Studio#!/about")
