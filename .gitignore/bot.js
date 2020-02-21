4e27d766130af3ef74aa0448a27a60cb3b0cd852
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2020-02-21 17:40:47 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -7,6 +7,9 @@ const client = new Discord.Client;
 const fs = require('fs');
 var prefix = "-";
 var normalcolor = "#1770E8";
+var GameName = "[⭐EVENT] Cookie Simulator 2!"
+var GroupDescription = "Our Roblox group is a vital part of the game. Our group is the way you get access to the discord and much more. There will be possible perks if you are in the group coming soon."
+var GameDescription = "Our game is one of the best games with state of the art security. We have invisible walls stopping exploiters, active staff to correctly punish people disobeying rules."
 client.login(process.env.TOKENBOT)
 
 // ---------------------------------------------------------------------------------------- //
@@ -20,8 +23,23 @@ client.on("message", function (message) {
     if (args[0].toLowerCase() === prefix + "group") {
         const GroupEmbed = new Discord.RichEmbed()
         .setTitle("The Shiny Studio")
-        .setDescription("Our Roblox group is a vital part of the game. Our group is the way you get access to the discord and much more. There will be possible perks if you are in the group coming soon.")
+        .setDescription(GroupDescription)
         .setURL("https://www.roblox.com/groups/5063409/The-Shiny-Studio#!/about")
+        .setTimestamp()
+        .setFooter("Requested by " + message.author.tag)
+        message.channel.send(GroupEmbed)
+    }
+})
+
+client.on("message", function (message) {
+    if (!message.guild) return
+    let args = message.content.trim().split(/ +/g)
+ 
+    if (args[0].toLowerCase() === prefix + "game") {
+        const GroupEmbed = new Discord.RichEmbed()
+        .setTitle(GameName)
+        .setDescription(GameDescription)
+        .setURL("https://www.roblox.com/games/4261816538/EVENT-Cookie-Simulator-2")
         .setTimestamp()
         .setFooter("Requested by " + message.author.tag)
         message.channel.send(GroupEmbed)
