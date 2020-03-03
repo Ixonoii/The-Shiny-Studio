e851b6c52a3cbfabd07c46caef8b3651218904fc
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2020-03-03 20:06:17 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -5,7 +5,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client;
 const fs = require('fs');
-var GuildId = "637704259105980416",
+var GuildId = "637704259105980416";
 var prefix = "-";
 var GameName = "[⭐EVENT] Cookie Simulator 2!";
 var GroupDescription = "Our Roblox group is a vital part of the game. Our group is the way you get access to the discord and much more. There will be possible perks if you are in the group coming soon.";
